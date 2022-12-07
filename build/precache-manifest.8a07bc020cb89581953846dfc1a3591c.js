242fd36e54047a7aeef71ea326ff6c3cb4065042
Jatin Saini <90445182+yunocrown@users.noreply.github.com>
2022-12-08 00:56:58 +0530

    Add files via upload

---
+++ build/precache-manifest.8a07bc020cb89581953846dfc1a3591c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "682a496b0f69554ac005",
+    "url": "/static/css/main.62386e77.chunk.css"
+  },
+  {
+    "revision": "682a496b0f69554ac005",
+    "url": "/static/js/main.682a496b.chunk.js"
+  },
+  {
+    "revision": "db8ffa8c117c110aa28b",
+    "url": "/static/js/1.db8ffa8c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da090369eb4126b43dcc249fdb187882",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { userCurrent } from "../hooks/userCurrent";
+
+export default function Profile(){
+    const { isLoading , isAuthorized , username } = userCurrent();
+    if (isLoading) return null;
+    if ( !isAuthorized ){
+        return (
+            <p>
+                You must <Link to='../pages/Desktop2'>login</Link>to view this page!
+            </p>
+        );
+    }
+    return(
+        <p>
+            You are signed in as <strong>{username}</strong>
+        </p>
+    );
+}
